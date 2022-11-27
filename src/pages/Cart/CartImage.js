3d2e3de94bf28a5aa0018f619172bd49e5175dfa
devlucaspg <110506094+devlucaspg@users.noreply.github.com>
2022-11-27 04:25:01 -0400

    feat: cart

--- src/pages/Cart/CartImage.js
+++ src/pages/Cart/CartImage.js
@@ -2,21 +2,33 @@ import styled from "styled-components";
 
 export default function CartImage({ name, price, imageLink, quantity }) {
   console.log(name, price, imageLink, quantity);
-    return (
+  return (
     <Conteiner>
       <img src={imageLink} alt={name} />
-      <p>{name}</p>
-      <p>R$ {price}</p>
-      <p>Quantidade: {quantity}</p>
+      <div>
+        <h1>{name}</h1>
+        <p>Qtd.: {quantity}</p>
+      </div>
+      <h2>R$ {price}</h2>
+
       <button>Remover</button>
     </Conteiner>
   );
 }
 
 const Conteiner = styled.div`
-  width: 100%;
+  width: 90%;
   display: flex;
-  img{
+  padding: 25px;
+  justify-content: space-between;
+  border: 1px solid black;
+  img {
     width: 90px;
+  }
+  p {
+    margin: 0 15px;
+    align-items: center;
+    justify-content: center;
+    padding: 40px 0;
   }
 `;
